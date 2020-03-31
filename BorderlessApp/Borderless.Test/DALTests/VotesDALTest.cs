f51bd84cc766c6196e82da5b7a28d20766eeaa2f
anaspoiala <ana.spoiala25@gmail.com>
2020-03-31 12:22:25 +0300

    Created the IDisposable class DbTestData. Implemented tests for add, update and delete in VotesDALTest and modified the existing tests to use DbTestData.

--- BorderlessApp/Borderless.Test/DALTests/VotesDALTest.cs
+++ BorderlessApp/Borderless.Test/DALTests/VotesDALTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Borderless.DataAccessLayer;
+using Borderless.Model.Entities;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -9,36 +10,114 @@ namespace Borderless.Test.DALTests
     [TestClass]
     public class VotesDALTest
     {
+        private static VotesDAL dal;
+
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            dal = new VotesDAL();
+        }
+
         [TestMethod]
         public void CanReadAll()
         {
-            var dal = new VotesDAL();
-            var votes = dal.ReadAll();
+            using (var data = new DbTestData())
+            {
+                var votes = dal.ReadAll();
 
-            votes.Should().NotBeNullOrEmpty();
+                votes.Should().NotBeNullOrEmpty();
+                votes.Should().HaveCountGreaterOrEqualTo(2);
+            }
         }
 
         [TestMethod]
         public void CanReadById()
         {
-            var dal = new VotesDAL();
-            var userId = new Guid("953D23D7-72D7-4C2A-9CC7-BF6FE8676094");
-            var translationId = new Guid("BB6502D2-8608-4A64-8F11-9A04DB11F1CB");
-            var vote = dal.ReadById(userId, translationId);
+            using (var data = new DbTestData())
+            {
+                var userId = data.user1.ID;
+                var translationId = data.translation2.ID;
+                var vote = dal.ReadById(userId, translationId);
 
-            vote.Should().NotBeNull();
-            vote.IsUpvote.Should().BeTrue();
+                vote.Should().NotBeNull();
+                vote.IsUpvote.Should().BeTrue();
+            }
+
         }
 
         [TestMethod]
         public void CanReadByTranslationId()
         {
-            var dal = new VotesDAL();
-            var translationId = new Guid("BB6502D2-8608-4A64-8F11-9A04DB11F1CB");
-            var votes = dal.ReadByTranslationId(translationId);
+            using (var data = new DbTestData())
+            {
+                var translationId = data.translation1.ID;
+                var votes = dal.ReadByTranslationId(translationId);
 
-            votes.Should().NotBeNullOrEmpty();
-            votes.Should().HaveCount(3);
+                votes.Should().NotBeNullOrEmpty();
+                votes.Should().HaveCount(1);
+            }
         }
+
+        [TestMethod]
+        public void CanAdd()
+        {
+            using (var data = new DbTestData())
+            {
+                Guid userId = data.user1.ID;
+                Guid translationId = data.translation1.ID;
+
+                dal.ReadById(userId, translationId).Should().BeNull();
+
+                var newVote = dal.Add(new Vote(userId, translationId, true));
+
+                dal.ReadById(userId, translationId).Should().NotBeNull();
+                newVote.Should().NotBeNull();
+                newVote.UserID.Should().Be(userId);
+                newVote.TranslationID.Should().Be(translationId);
+                newVote.IsUpvote.Should().BeTrue();
+
+                dal.DeleteById(newVote.UserID, newVote.TranslationID);
+            }
+        }
+
+        [TestMethod]
+        public void CanUpdate()
+        {
+            using (var data = new DbTestData())
+            {
+                Guid userId = data.user1.ID;
+                Guid translationId = data.translation2.ID;
+
+                var vote = dal.ReadById(userId, translationId);
+                vote.IsUpvote.Should().BeTrue();
+
+                vote.IsUpvote = false;
+                var updatedVote = dal.UpdateById(userId, translationId, vote);
+
+                dal.ReadById(userId, translationId).Should().NotBeNull();
+                updatedVote.Should().NotBeNull();
+                updatedVote.UserID.Should().Be(userId);
+                updatedVote.TranslationID.Should().Be(translationId);
+                updatedVote.IsUpvote.Should().BeFalse();
+            }
+        }
+
+        [TestMethod]
+        public void CanDelete()
+        {
+            using (var data = new DbTestData())
+            {
+                Guid userId = data.user1.ID;
+                Guid translationId = data.translation2.ID;
+
+                var deletedVote = dal.ReadById(userId, translationId);
+                deletedVote.Should().NotBeNull();
+
+                dal.DeleteById(deletedVote.UserID, deletedVote.TranslationID);
+
+                dal.ReadById(userId, translationId).Should().BeNull();
+            }
+        }
+
     }
 }
---
+++ BorderlessApp/Borderless.Test/DbTestData.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using Borderless.DataAccessLayer;
+using Borderless.Model.Entities;
+
+namespace Borderless.Test
+{
+    public class DbTestData : IDisposable
+    {
+        public User user1;
+        public User user2;
+        public Project project1;
+        public Project project2;
+        public Language language1;
+        public Language language2;
+        public Phrase phrase1;
+        public Phrase phrase2;
+        public Translation translation1;
+        public Translation translation2;
+        public Vote vote1;
+        public Vote vote2;
+
+        private UsersDAL _usersDAL;
+        private LanguagesDAL _languagesDAL;
+        private ProjectsDAL _projectsDAL;
+        private PhrasesDAL _phrasesDAL;
+        private TranslationsDAL _translationsDAL;
+        private VotesDAL _votesDAL;
+
+        public DbTestData()
+        {
+            InitDAL();
+
+            InitUsers();
+            InitLanguages();
+            InitProjects();
+            InitPhrases();
+            InitTranslations();
+            InitVotes();
+        }
+
+
+        public void Dispose()
+        {
+            DisposeVotes();
+            DisposeTranslations();
+            DisposePhrases();
+            DisposeProjects();
+            DisposeUsers();
+        }
+
+
+        private void InitDAL()
+        {
+            _usersDAL = new UsersDAL();
+            _languagesDAL = new LanguagesDAL();
+            _projectsDAL = new ProjectsDAL();
+            _phrasesDAL = new PhrasesDAL();
+            _translationsDAL = new TranslationsDAL();
+            _votesDAL = new VotesDAL();
+        }
+
+        private void InitUsers()
+        {
+            user1 = _usersDAL.Add(new User(Guid.Empty, "dummyUser1", "8ee2027983915ec78acc45027d874316", "Jack", "Jones", "test1@email.com"));
+            user2 = _usersDAL.Add(new User(Guid.Empty, "dummyUser2", "8ee2027985915ec78acc45067d874317", "Helen", "Rice", "test2@email.com"));
+        }
+
+        private void InitLanguages()
+        {
+            language1 = _languagesDAL.ReadById(new Guid("459A4220-416C-4100-B608-6EEDD49E8E91"));  // english
+            language2 = _languagesDAL.ReadById(new Guid("24653028-8AE0-47FE-B4B5-046C904C56DE"));  // german
+        }
+
+        private void InitProjects()
+        {
+            project1 = _projectsDAL.Add(new Project(Guid.Empty, user1.ID, "Project 1", "", language1, new List<Language> { language2 }));
+            project2 = _projectsDAL.Add(new Project(Guid.Empty, user2.ID, "Project 2", "", language2, new List<Language> { language1 }));
+        }
+
+        private void InitPhrases()
+        {
+            phrase1 = _phrasesDAL.Add(new Phrase(Guid.Empty, project1.ID, "test phrase 1"));
+            phrase2 = _phrasesDAL.Add(new Phrase(Guid.Empty, project2.ID, "test phrase 2"));
+        }
+
+        private void InitTranslations()
+        {
+            translation1 = _translationsDAL.Add(new Translation(Guid.Empty, "translation 1", phrase1.ID, language2.ID, user2.ID));
+            translation2 = _translationsDAL.Add(new Translation(Guid.Empty, "translation 2", phrase2.ID, language1.ID, user1.ID));
+        }
+
+        private void InitVotes()
+        {
+            vote1 = _votesDAL.Add(new Vote(user1.ID, translation2.ID, true));
+            vote2 = _votesDAL.Add(new Vote(user2.ID, translation1.ID, false));
+        }
+
+        private void DisposeUsers()
+        {
+            _usersDAL.DeleteById(user1.ID);
+            _usersDAL.DeleteById(user2.ID);
+        }
+
+
+        private void DisposeProjects()
+        {
+            _projectsDAL.DeleteById(project1.ID);
+            _projectsDAL.DeleteById(project2.ID);
+        }
+
+        private void DisposePhrases()
+        {
+            _phrasesDAL.DeleteById(phrase1.ID);
+            _phrasesDAL.DeleteById(phrase2.ID);
+        }
+
+        private void DisposeTranslations()
+        {
+            _translationsDAL.DeleteById(translation1.ID);
+            _translationsDAL.DeleteById(translation2.ID);
+        }
+
+        private void DisposeVotes()
+        {
+            _votesDAL.DeleteById(vote1.UserID, vote1.TranslationID);
+            _votesDAL.DeleteById(vote2.UserID, vote2.TranslationID);
+        }
+    }
+}
